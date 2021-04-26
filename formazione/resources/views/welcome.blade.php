0705380e19abbdb5f7fe7213ca5bd235bf8fa3cf
massimo.angelico <massimo@masteritalia.it>
2021-04-26 10:32:48 +0200

    Commit dei file Eugenio e Gian

--- formazione/resources/views/welcome.blade.php
+++ formazione/resources/views/welcome.blade.php
@@ -38,6 +38,7 @@
             @endif
 
             <h2>Hello by Master Italia!!</h2>
+            <h3>By Massimo </h2>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
