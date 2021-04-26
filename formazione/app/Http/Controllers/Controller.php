1d192469e71736c76d1e8038f44567c8110ea950
eugenio.franchin <eugenio@masteritalia.it>
2021-04-26 10:32:49 +0200

    modifiche eugenio

--- formazione/app/Http/Controllers/Controller.php
+++ formazione/app/Http/Controllers/Controller.php
@@ -13,4 +13,5 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    dd( 'Test Eugenio' );
 }
